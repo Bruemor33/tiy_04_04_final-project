ad0402b59d9984c1b87d49cf336537d2b4fb69b6
Bruemor <arymer1@gmail.com>
2016-04-05 16:18:33 -0400

    Begain building out the dependencies of the components I'll be using.

--- app/scripts/components/login.jsx
+++ app/scripts/components/login.jsx
@@ -5,5 +5,4 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Parse = require('parse');
 var ParseReact = require('parse-react');
-
-console.log("Hello World!");
+require('backbone-React-Component');
--- app/scripts/components/signup.jsx
+++ app/scripts/components/signup.jsx
@@ -4,6 +4,6 @@ var Backbone = require('backbone');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Parse = require('parse');
+require('backbone-react-component');
+var LinkedStateMixin = require('react/lib/LinkedStateMixin');
 var ParseReact = require('parse-react');
-
-console.log("Hello World!");

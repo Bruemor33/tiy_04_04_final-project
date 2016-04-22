1028434b0cf1f725fdf294b39597b85c59c150e5
Bruemor <arymer1@gmail.com>
2016-04-22 03:02:05 -0400

    re-arranged some of the xml in order to more freely style

--- app/scripts/components/component-form-page.jsx
+++ app/scripts/components/component-form-page.jsx
@@ -18,20 +18,90 @@ var ComponentForms = React.createClass({
         <div className="component-form-list col-md-12">
           <h1 className="part-selection">Component form Selection</h1>
           <ul className="part-list">
-            <li className="part-item"><a href={"#chain"}>Chains</a></li>
-            <li className="part-item"><a href={"#chainring"}>Chainrings</a></li>
-            <li className="part-item"><a href={"#cranks"}>Cranksets</a></li>
-            <li className="part-item"><a href={"#bottombracket"}>Bottombrackets</a></li>
-            <li className="part-item"><a href={"#clip"}>Foot Retention</a></li>
-            <li className="part-item"><a href={"#pedal"}>Pedals</a></li>
-            <li className="part-item"><a href={"#saddle"}>Saddles</a></li>
-            <li className="part-item"><a href={"#seatpost"}>Seatposts</a></li>
-            <li className="part-item"><a href={"#headset"}>Headsets</a></li>
-            <li className="part-item"><a href={"#stem"}>Stems</a></li>
-            <li className="part-item"><a href={"#handle"}>Handlebars</a></li>
-            <li className="part-item"><a href={"#frame"}>Frames</a></li>
-            <li className="part-item"><a href={"#wheel"}>Wheelsets</a></li>
-            <li className="part-item"><a href={"#tire"}>Tires</a></li>
+            <li className="part-item">
+              <a href={"#chain"}>
+                <p className="form-title">Chains</p>
+                <p className="form-description">- add an additional chain to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#chainring"}>
+                <p className="form-title">Chainrings</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#cranks"}>
+                <p className="form-title">Cranksets</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#bottombracket"}>
+                <p className="form-title">Bottombrackets</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#clip"}>
+                <p className="form-title">Foot Retention</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#pedal"}>
+                <p className="form-title">Pedals</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#saddle"}>
+                <p className="form-title">Saddles</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#seatpost"}>
+                <p className="form-title">Seatposts</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#headset"}>
+                <p className="form-title">Headsets</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#stem"}>
+                <p className="form-title">Stems</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#handle"}>
+                <p className="form-title">Handlebars</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#frame"}>
+                <p className="form-title">Frames</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#wheel"}>
+                <p className="form-title">Wheelsets</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
+            <li className="part-item">
+              <a href={"#tire"}>
+                <p className="form-title">Tires</p>
+                <p className="form-description"> add additional ________ to the collection.</p>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
